9307b5b76c99ec8029cb09ce86d7134fd7a4f3b9
Padmavathi na <bkumar1675@gmail.com>
2020-09-06 21:59:31 -0400

    template driven form-angular

---
+++ tdf/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics=['Angular','React','Vue'];
+  topicHasError=true;
+  userModel = new User('Rob','rob@test.com','5555556555', 'default','morning',true);
+  
+   validateTopic(value){
+     if(value=='default'){
+       this.topicHasError=true;
+     }else{
+       this.topicHasError=false;
+     }
+  
+  }
+
+}
+
